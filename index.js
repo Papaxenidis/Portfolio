f06addf6f54a70fff2b44d7b7c2a0b135f98f77e
Papaxenidis <lord.papax@gmail.com>
2023-12-18 10:37:37 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let mybutton = document.querySelector("#first");
+
+function hey(){
+
+        window.scrollTo(0, 3000);
+      
+
+
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo(0, 500);
+
+}
